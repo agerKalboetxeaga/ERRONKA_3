847c86181ccd6a40bdfb9a007406a532c753cba3
agerKalboetxeaga <kalboetxeaga.ager@uni.eus>
2021-12-20 13:04:38 +0100

    partidak_module

---
+++ erronka_DOCS/odoo/partidak/models/models.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+# from odoo import models, fields, api
+
+
+# class partidak(models.Model):
+#     _name = 'partidak.partidak'
+#     _description = 'partidak.partidak'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
+
+from odoo import models, fields, api
+
+class partida(models.Model):
+	_name = "partidak.partida"
+	_description = "Partida baten ezaugarriak ezartzen digun modeloa."
+	_order = 'puntuazioa'
+
+	#clave que ve el usuario (string='')
+	#name= fields.Char(string = "Nombre", required=True,)
+
+	#partner_name = fields.Many2one('res.partner', string='name')
+	puntuazioa = fields.Integer(string='Puntuazioa')
+	kills = fields.Integer(string='Kills')
+	time = fields.Char(string='Run Time')
+	date = fields.Datetime(string='Fecha de la azaña')
+	res_partner_id = fields.Many2one('res.partner', string='Employee')
+
+	#Relacion entre tablas
+	 # puede petar
+	
+	#Possible future feature...
+	#manquismo= fields.Char(string='Eres malo pero...', compute='imBetter')
+
+
+
