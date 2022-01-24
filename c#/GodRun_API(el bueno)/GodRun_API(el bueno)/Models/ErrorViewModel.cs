e45a2aedfdf0fc5a7aad3ebeef959efb1e76492b
kalboetxeaga.ager <kalboetxeaga.ager@uni.eus>
2022-01-24 10:12:30 +0100

    limpiando algo

---
+++ c#/GodRun_API(el bueno)/GodRun_API(el bueno)/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GodRun_API_el_bueno_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
