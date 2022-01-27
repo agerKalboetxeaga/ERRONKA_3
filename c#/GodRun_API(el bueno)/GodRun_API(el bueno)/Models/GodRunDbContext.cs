626aee9d14ba98bbd987353e2fc1916ba9acb1e4
perezkepa <90681065+perezkepa@users.noreply.github.com>
2022-01-27 08:29:43 +0100

    kepainkesta

---
+++ c#/GodRun_API(el bueno)/GodRun_API(el bueno)/Models/GodRunDbContext.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GodRun_API_el_bueno_.Models
+{
+    public class GodRunDbContext : DbContext
+    {
+        public DbSet<Inkesta> Inkesta { get; set; }
+
+        public GodRunDbContext(DbContextOptions<GodRunDbContext> options) //sino peta
+    : base(options)
+        { }
+        /*
+        public GodRunDbContext(DbContextOptions<GodRunDbContext> dbContextOptions)
+        {
+            
+        }
+        */
+    }
+}
